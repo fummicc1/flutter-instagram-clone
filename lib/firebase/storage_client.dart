9ffe230e1fcabfbd25561601d4738f84cbe5640e
速川湧気 <hykwyuk0125@gmail.com>
2021-08-08 17:13:02 +0900

    StorageClientの作成(Upload/Download)

---
+++ lib/firebase/storage_client.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+
+class StorageClient {
+  final FirebaseStorage _storage;
+  StorageClient(this._storage);
+
+  // ファイルをアップロードする
+  // Taskを返すけど使い方がよくわかっていない
+  Future<UploadTask?> uploadFile(
+    File? file,
+    String storePath,
+    String contentType,
+  ) async {
+    if (file == null) {
+      return Future.error("Invalid File Path");
+    }
+
+    final ref = _storage.ref(storePath);
+    final metadata = SettableMetadata(contentType: contentType);
+
+    return Future.value(ref.putFile(file, metadata));
+  }
+
+  // ダウンロード先の一時ファイルを返す
+  Future<File?> downloadFile(Reference ref) async {
+    final Directory systemTempDir = Directory.systemTemp;
+    final File tempFile = File("${systemTempDir.path}/temp-${ref.name}");
+    if (tempFile.existsSync()) await tempFile.delete();
+
+    var result = await ref.writeToFile(tempFile);
+    if (result.state == TaskState.success) {
+      return tempFile;
+    }
+    return Future.error("Failed to download");
+  }
+
+  // Image.network()で直接セットできるURLを取得する
+  Future<String?> getLink(Reference ref) async {
+    return await ref.getDownloadURL();
+  }
+
+  Future<Reference> getStorageRef(String dir, String fileName) async {
+    return _storage.ref("$dir/$fileName");
+  }
+}
