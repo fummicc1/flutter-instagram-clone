d775870c7f5b175e748328156c6d526069d18acd
fummicc1 <fumiya.tennis1@gmail.com>
2021-08-16 00:46:59 +0900

    Create FakePostRepository

---
+++ test/fake/repositories/fake_post_repository.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_instagram/entities/post.dart';
+import 'package:flutter_instagram/repositories/post_repository.dart';
+import 'package:flutter_instagram/repositories/query.dart';
+
+class FakePostRepository extends IPostRepository {
+
+  Map<DocumentReference, PostEntity> _data = {};
+
+  @override
+  Future create(PostEntity postEntity) async {
+    final ref = FirebaseFirestore.instance.collection(PostEntity.collectionName).doc(postEntity.id);
+    _data[ref] = postEntity;
+  }
+
+  @override
+  Future<List<PostEntity>> findWithQueries(List<QueryModel> queries) async {
+    var data = _data;
+
+    for (QueryModel queryModel in queries) {
+      if (queryModel is EqualQueryModel) {
+        final fieldName = queryModel.fieldName;
+        final fieldValue = queryModel.fieldValue;
+        data.removeWhere((key, value) => value.data[fieldName] != fieldValue);
+      } else if (queryModel is ContainsQueryModel) {
+        final fieldName = queryModel.fieldName;
+        final element = queryModel.element;
+        data.removeWhere( (key, value) {
+          final list = value.data[fieldName] as List;
+          return list.contains(element);
+        });
+      }
+    }
+
+    return data.values.toList();
+  }
+
+  @override
+  Future<List<PostEntity>> findWithQuery(QueryModel query) async {
+    return findWithQueries([query]);
+  }
+
+  @override
+  Future update({required String id, required PostEntity newEntity}) async {
+    final ref = FirebaseFirestore.instance.collection(PostEntity.collectionName).doc(id);
+    _data[ref] = newEntity;
+  }
+}
