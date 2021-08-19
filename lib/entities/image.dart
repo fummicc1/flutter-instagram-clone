7a4b702bcd3eb70bca3e74ff58f24ce11c743dfb
fummicc1 <fumiya.tennis1@gmail.com>
2021-08-20 00:44:22 +0900

    Add implementation

--- lib/entities/image.dart
+++ lib/entities/image.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram/common/exception.dart';
 
 @immutable
 class ImageEntity {
@@ -6,4 +7,21 @@ class ImageEntity {
   final String storageRef;
 
   ImageEntity({required this.id, required this.storageRef});
+
+  factory ImageEntity.fromData(Map<String, dynamic> data) {
+    final id = data["id"] as String?;
+    final storageRef = data["storage_ref"] as String?;
+    if (id == null || storageRef == null) {
+      throw EntityParserException(data);
+    }
+    return ImageEntity(
+        id: id,
+        storageRef: storageRef
+    );
+  }
+
+  Map<String, dynamic> get data => {
+    "id": id,
+    "storage_ref": storageRef
+  };
 }
--- lib/repositories/image_repository.dart
+++ lib/repositories/image_repository.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_instagram/entities/image.dart';
+import 'package:flutter_instagram/firebase/firestore_client.dart';
+import 'package:flutter_instagram/firebase/storage_client.dart';
 import 'package:flutter_instagram/repositories/query.dart';
 
 abstract class IImageRepository {
@@ -10,10 +15,63 @@ abstract class IImageRepository {
   Future<List<ImageEntity>> findWithQueries(List<QueryModel> queries);
 
   /// Get url to download image from File-Storage
-  Future<String> getURL();
+  Future<String> getURL(String ref);
 
   /// Create new resource
-  Future create(ImageEntity imageEntity);
+  Future create({required ImageEntity imageEntity, required File file});
   /// Delete existing resource
-  Future delete(ImageEntity imageEntity);
+  Future delete({required ImageEntity imageEntity, File? file});
+}
+
+class ImageRepository implements IImageRepository {
+
+  final FirestoreClient _firestoreClient;
+  final StorageClient _storageClient;
+
+  static const _collectionName = "images";
+
+  ImageRepository(this._firestoreClient, this._storageClient);
+
+  @override
+  Future create(ImageEntity imageEntity) async {
+    try {
+      final data = imageEntity.data;
+       await _firestoreClient.createDoc(collectionName: _collectionName, data: data);
+
+       _storageClient.uploadFile(file: file, path: path, contentType: contentType)
+    } catch {
+
+    }
+  }
+
+  @override
+  Future delete(ImageEntity imageEntity) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ImageEntity> find(String id) {
+    // TODO: implement find
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ImageEntity>> findWithQueries(List<QueryModel> queries) {
+    // TODO: implement findWithQueries
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ImageEntity>> findWithQuery(QueryModel queryModel) {
+    // TODO: implement findWithQuery
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> getURL(String ref) async {
+    final Reference ref = _storageClient.getStorageRef(ref);
+    _storageClient.downloadFile(ref)
+  }
+
 }
