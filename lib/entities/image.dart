783faecf7e670400fb1d470cba71a355c2fc9a96
fummicc1 <fumiya.tennis1@gmail.com>
2021-08-20 13:36:59 +0900

    Add types

--- lib/entities/image.dart
+++ lib/entities/image.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
+enum ImageFileType { PNG, JPEG }
+
+@immutable
+class ImageMetadata {
+  final ImageFileType fileType;
+
+  ImageMetadata({required this.fileType});
+
+  String? get contentType {
+    if (fileType == ImageFileType.JPEG) {
+      return "image/jpeg";
+    } else if (fileType == ImageFileType.PNG) {
+      return "image/png";
+    }
+    return null;
+  }
+}
+
 @immutable
 class ImageEntity {
   final String? id;
   final String storageRef;
-  final String contentType;
+  final ImageMetadata imageMetadata;
 
-  ImageEntity({required this.id, required this.storageRef, required this.contentType});
+  ImageEntity(
+      {required this.id,
+      required this.storageRef,
+      required this.imageMetadata});
 }
