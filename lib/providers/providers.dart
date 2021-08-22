55af7fdbb49e1a83bab0d8830edd3db3abf941e1
速川湧気 <hykwyuk0125@gmail.com>
2021-08-22 22:30:29 +0900

    create errorStateProvider

---
+++ lib/providers/providers.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_instagram/common/exception.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+/// flow errors from ViewModels.
+final errorStateProvider = StateProvider<GenericException?>((_) => null);
