08cb0d00148bcd28f6d8ebb0e586446301c91587
fummicc1 <fumiya.tennis1@gmail.com>
2021-08-24 18:23:40 +0900

    fix https://github.com/fummicc1/flutter-instagram-clone/pull/62#discussion_r694545141

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_instagram/common/exception.dart';
 import 'package:flutter_instagram/firebase/auth_client.dart';
 import 'package:flutter_instagram/firebase/firestore_client.dart';
@@ -15,39 +14,39 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final errorStateProvider = StateProvider<GenericException?>((_) => null);
 
 /// IFirestoreClient
-final firestoreClientProvider = StateProvider<IFirestoreClient>((ref) {
+final _firestoreClient = Provider<IFirestoreClient>((ref) {
   final FirestoreClient firestoreClient =
       FirestoreClient(FirebaseFirestore.instance);
   return firestoreClient;
 });
 
 /// IStorageClient
-final storageClientProvider = StateProvider<IStorageClient>((ref) {
+final _storageClient = Provider<IStorageClient>((ref) {
   final StorageClient storageClient = StorageClient(FirebaseStorage.instance);
   return storageClient;
 });
 
 /// IAuthClient
-final authClientProvider = StateProvider<IAuthClient>((ref) {
+final _authClient = Provider<IAuthClient>((ref) {
   final AuthClient authClient = AuthClient(FirebaseAuth.instance);
   return authClient;
 });
 
 /// Repositories
-final imageRepositoryProvider = StateProvider<IImageRepository>((ref) {
-  final firestore = ref.watch(firestoreClientProvider).state;
-  final storage = ref.watch(storageClientProvider).state;
+final _imageRepository = Provider<IImageRepository>((ref) {
+  final firestore = ref.watch(_firestoreClient);
+  final storage = ref.watch(_storageClient);
 
   final IImageRepository imageRepository = ImageRepository(firestore, storage);
   return imageRepository;
 });
 
-final postRepositoryProvider = StateProvider<IPostRepository>((ref) {
-  final firestore = ref.watch(firestoreClientProvider).state;
+final _postRepository = StateProvider<IPostRepository>((ref) {
+  final firestore = ref.watch(_firestoreClient);
   return PostRepository(firestore);
 });
 
-final userRepositoryProvider = StateProvider<IUserRepository>((ref) {
-  final firestore = ref.watch(firestoreClientProvider).state;
+final _userRepository = StateProvider<IUserRepository>((ref) {
+  final firestore = ref.watch(_firestoreClient);
   return UserRepository(firestore);
 });
