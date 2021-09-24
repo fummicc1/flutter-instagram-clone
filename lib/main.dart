fabb3b5bfb3ce5dafb3b8cf26d07736874279a82
速川湧気 <hykwyuk0125@gmail.com>
2021-09-24 23:37:49 +0900

    migrate theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(ProviderScope(
-      child: FastTheme(
-    accentColor: Colors.blue,
-    themeMode: ThemeMode.light,
-    child: MyApp(),
-  )));
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: FastTheme.of(context).theme,
-        home: RootPage());
+    return FastThemeScope(
+      accentColor: Colors.blue,
+      themeMode: ThemeMode.light,
+      builder: (context, lightTheme, darkTheme, mode) {
+        return MaterialApp(
+            title: 'Flutter Demo',
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode: mode,
+            home: RootPage());
+      },
+    );
   }
 }
